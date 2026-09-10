c5576cbd9203bc258a9e82656925b398eedde61c
mardaxrakys <7emetriy@gmail.com>
2026-09-10 19:09:40 +0300

    2.1_test

--- mine.py
+++ mine.py
@@ -10,4 +10,14 @@ print ("Hello Git") # lox
 # Виведення (виведення даних)
 print(вітання)
 
+a = input("Рядок запрошення: ")
+# На екрані ви побачите: Рядок запрошення:
+age = input("How old are you? ")
+age = int(age)
+
+pi = float('3.14')
+
+pi_str = str(3.14)
+age_str = str(29)
+
 
